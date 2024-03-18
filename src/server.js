2f45f142fb05b442dee843297458fa1fb88be006
Ariosi Lucas <ariosilucas@gmail.com>
2024-03-18 11:47:15 -0300

    Explaining how to use the Method GET

--- src/server.js
+++ src/server.js
@@ -1,6 +1,16 @@
+// Ele é usado para fazer as API, ele é o garçom, e nós definimos a porta ali abaixo no const PORT para ele saber em qual restaurante (Porta do localhost) que ele vai trabalhar.
 const express = require("express");
 
+//Aqui é onde colocamos oque queremos fazer em certa parte da API.
 const app = express();
 
+//Utilizmos o .get para selecionarmos qual será a rota que aparecerá certa coisa para o usuário, exemplo se eu colocar ali no lugar do "/" um "/message" então no seu browser você tem que colocar o localhost:3333/message para que apareça o Hello World! 
+//Utilizando o response.send serve para gerarmos uma resposta, para enviarmos algo pra dentro do site em que estamos criando, e nesse caso ele é utilizado para escrever apenas Hello World.
+app.get("/", (request, response) => {
+   response.send("Hello World")
+});
+
+
+// Pra ele sempre ficar observando a porta que ele irá realizar as funções e outras coisas que adicionarmos à API utilizamos o Listen
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
