f9fe1549e80e9ef1768aff46209c17cc753da8a8
Ariosi Lucas <ariosilucas@gmail.com>
2024-03-18 15:10:30 -0300

    HTTP Codes

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -2,8 +2,10 @@ class UsersController {
    create(request, response){
       const { name, email, password} = request.body
 
-      response.json({ name, email, password })
+      response.status(201).json({ name, email, password })
    }
 }
 
 module.exports = UsersController;
+
+// Linha 05, utilizamos o status() para que possamos adicionar informações a mais no nosso sistema, nesse caso ele adiciona status do sistema, existem varios status que podemos colocar, desde 200 a 400 e 500, nesse caso da linha 5 usamos o 201 para o status de criado.
