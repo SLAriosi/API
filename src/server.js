ed108ba1dd07d59d8be08afe79a9f0ddbaa25977
Ariosi Lucas <ariosilucas@gmail.com>
2024-03-18 15:53:46 -0300

    Treating Exceptions

--- src/server.js
+++ src/server.js
@@ -1,3 +1,6 @@
+require("express-async-errors");
+
+const AppError = require("./utils/AppError")
 // Ele é usado para fazer as API, ele é o garçom, e nós definimos a porta ali abaixo no const PORT para ele saber em qual restaurante (Porta do localhost) que ele vai trabalhar.
 const express = require("express")
 
@@ -22,6 +25,22 @@ app.get("/message/:id/:user", (request, response) => {
 //Esse app.use é necessário utilizar para que o sistema entenda que vamos passar essas informações por meio de uma expressão JSON(), ou outra dependendo do que estamos trabalhando, se não utilizarmos vai dar erro
 app.use(express.json())
 app.use(routes)
+app.use(( error, request, response, next) => {
+   if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+         status: "error",
+         message: error.message
+      })
+   }
+
+   console.error(error)
+
+   return response.status(500).json({
+      status: "error",
+      message: "Internal server error",
+   })
+
+})
 
 // Pra ele sempre ficar observando a porta que ele irá realizar as funções e outras coisas que adicionarmos à API utilizamos o Listen
 const PORT = 3333
