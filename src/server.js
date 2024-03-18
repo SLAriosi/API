b9e448b622fbb4fac0fc9531c06e26f14646e961
Ariosi Lucas <ariosilucas@gmail.com>
2024-03-18 14:53:26 -0300

    Organizing the structure of project

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 // Ele é usado para fazer as API, ele é o garçom, e nós definimos a porta ali abaixo no const PORT para ele saber em qual restaurante (Porta do localhost) que ele vai trabalhar.
 const express = require("express")
 
+const routes = require("./routes")
 //Aqui é onde colocamos oque queremos fazer em certa parte da API.
 const app = express()
-
 //Utilizmos o .get para selecionarmos qual será a rota que aparecerá certa coisa para o usuário, exemplo se eu colocar ali no lugar do "/" um "/message" então no seu browser você tem que colocar o localhost:3333/message para que apareça o Hello World! 
 app.get("/message", (request, response) => {
    response.send("Hello World")
@@ -13,21 +13,16 @@ app.get("/message", (request, response) => {
 app.get("/message/:id/:user", (request, response) => {
    // Voce pode desestruturar o request.params aqui dessa maneira para que no ${request.params.id} & ${request.params.user} voce possa colocar apenas ${id} e ${user}
    const { id, user } = request.params
-
+   
    response.send(`
-      ID da Mensagem: ${id}.
-      Para o usuário ${user}.
+   ID da Mensagem: ${id}.
+   Para o usuário ${user}.
    `)
 })
 //Esse app.use é necessário utilizar para que o sistema entenda que vamos passar essas informações por meio de uma expressão JSON(), ou outra dependendo do que estamos trabalhando, se não utilizarmos vai dar erro
 app.use(express.json())
-
-app.post("/users", (request, response) => {
-   const { name, email, password} = request.body
-
-   response.json({ name,email,password })
-})
+app.use(routes)
 
 // Pra ele sempre ficar observando a porta que ele irá realizar as funções e outras coisas que adicionarmos à API utilizamos o Listen
 const PORT = 3333
-app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
