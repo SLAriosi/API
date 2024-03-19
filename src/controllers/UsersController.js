3e7a4823c97d381f1a7f7774b73bb3955b7a0c7a
Ariosi Lucas <ariosilucas@gmail.com>
2024-03-19 08:27:43 -0300

    Select Added

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,8 +1,17 @@
 const AppError = require("../utils/AppError")
 
+const sqliteConnection = require("../database/sqlite")
+
 class UsersController {
-   create(request, response){
+   async create(request, response){
       const { name, email, password} = request.body;
+
+      const database = await sqliteConnection();
+      const checkUserExist = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+      if(checkUserExist){
+         throw new AppError("Este e-mail já está em uso.");
+      }
 
       if (!name) {
          throw new AppError("Nome é obrigatório!!")
@@ -14,4 +23,4 @@ class UsersController {
 
 module.exports = UsersController;
 
-// Linha 05, utilizamos o status() para que possamos adicionar informações a mais no nosso sistema, nesse caso ele adiciona status do sistema, existem varios status que podemos colocar, desde 200 a 400 e 500, nesse caso da linha 5 usamos o 201 para o status de criado.
+// Linha 09, utilizamos o status() para que possamos adicionar informações a mais no nosso sistema, nesse caso ele adiciona status do sistema, existem varios status que podemos colocar, desde 200 a 400 e 500, nesse caso da linha 5 usamos o 201 para o status de criado.
