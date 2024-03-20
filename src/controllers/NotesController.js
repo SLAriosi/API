dd95169be00e06113291dc413f02557b1fbe115f
Ariosi Lucas <ariosilucas@gmail.com>
2024-03-20 11:43:19 -0300

    Operator Like / using .whereLike('variavel que quer ver', %%)

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -56,10 +56,11 @@ class NotesController{
    }
 
    async index(request, response){
-      const { user_id } = request.query
+      const { user_id, title } = request.query
 
       const notes = await knex("notes")
       .where({ user_id })
+      .whereLike("title", `%${title}%`)
       .orderBy("title")
 
       return response.json(notes);
