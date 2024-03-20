d23c4a368a3bdbb4d7d2e68eeaa40f34cb23c721
Ariosi Lucas <ariosilucas@gmail.com>
2024-03-20 14:42:04 -0300

    Map and Filter

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -61,11 +61,19 @@ class NotesController {
       let notes;
 
       if (tags) {
-
          const filterTags = tags.split(',').map(tag => tag.trim())
 
          notes = await knex("tags")
+            .select([
+               "notes.id",
+               "notes.title",
+               "notes.user_id",
+            ])
+            .where("notes.user_id", user_id)
+            .whereLike("notes.title", `%${title}%`)
             .whereIn("name", filterTags)
+            .innerJoin("notes", "notes.id", "tags.note_id")
+            .orderBy("notes.title")
 
       } else {
 
