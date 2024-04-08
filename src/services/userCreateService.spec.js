194456c7d33f42442c303cd20d687528e67d2655
Ariosi Lucas <ariosilucas@gmail.com>
2024-04-08 15:51:12 -0300

    Creating Test Using Repository in Memory, About Creating a new User

--- src/services/userCreateService.spec.js
+++ src/services/userCreateService.spec.js
@@ -1,4 +1,6 @@
-const UserCreateService = require("./UserCreateService")
+const UserCreateService = require("./UserCreateService");
+const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory")
+
 it ("User should be created", async () => {
    
    const user = {
@@ -7,7 +9,8 @@ it ("User should be created", async () => {
       password: "123"
    };
 
-   const userCreateService = new UserCreateService();
+   const userRepositoryInMemory = new UserRepositoryInMemory();
+   const userCreateService = new UserCreateService(userRepositoryInMemory);
    const userCreated = await userCreateService.execute(user);
 
    expect(userCreated).toHaveProperty("id");
