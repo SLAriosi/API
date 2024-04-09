382c53d3bd7a99ac5b066fdb76b98e4c76461376
Ariosi Lucas <ariosilucas@gmail.com>
2024-04-09 09:04:58 -0300

    Finally finishing the course, After used beforeEach, and fixing the tests methods

--- src/services/userCreateService.spec.js
+++ src/services/userCreateService.spec.js
@@ -1,17 +1,46 @@
 const UserCreateService = require("./UserCreateService");
-const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory")
+const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory");
+const AppError = require("../utils/AppError");
 
-it ("User should be created", async () => {
-   
-   const user = {
-      name: "User test",
-      email: "usertest@gmail.com",
-      password: "123"
-   };
+// Usar o Describe ajuda a separar os testes em tipos de grupos diferentes, e na hora que aparecer o erro, estará escrito o local correto mostrando oque está errado, e no grupo que está o erro.
+// O Describe é normalmente utilizado, apenas 1 por arquivo de teste.
+describe("UserCreateService", () => {
 
-   const userRepositoryInMemory = new UserRepositoryInMemory();
-   const userCreateService = new UserCreateService(userRepositoryInMemory);
-   const userCreated = await userCreateService.execute(user);
+   let userRepositoryInMemory = null;
+   let userCreateService = null;
 
-   expect(userCreated).toHaveProperty("id");
-});
+   // Esse before each executa antes de cada teste, permitindo que acessemos essas informações dentro de cada um dos testes, nesse caso podemos usar o userRepositoryInMemory & o userCreateService.
+   beforeEach(() => {
+      userRepositoryInMemory = new UserRepositoryInMemory();
+      userCreateService = new UserCreateService(userRepositoryInMemory);
+   })
+
+   it("User should be created", async () => {
+
+      const user = {
+         name: "User test",
+         email: "usertest@gmail.com",
+         password: "123"
+      };
+
+      const userCreated = await userCreateService.execute(user);
+      expect(userCreated).toHaveProperty("id");
+   });
+
+   it("User NOT should be created w/ same email than another", async () => {
+      const user1 = {
+         name: "User Test 1",
+         email: "user@test.com",
+         password: "123"
+      }
+      
+      const user2 = {
+         name: "User Test 2",
+         email: "user@test.com",
+         password: "456"
+      }
+
+      await userCreateService.execute(user1);
+      await expect(userCreateService.execute(user2)).rejects.toEqual(new AppError("Este e-mail já está em uso."));
+   })
+})
